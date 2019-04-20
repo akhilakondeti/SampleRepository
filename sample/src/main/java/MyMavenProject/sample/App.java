a2c6565e8e78be78681188a007977c56ec6ff50f
akhila kondeti <akhila.k@ggktech.com>
2019-04-20 15:07:45 +0530

    chagned

--- sample/src/main/java/MyMavenProject/sample/App.java
+++ sample/src/main/java/MyMavenProject/sample/App.java
@@ -8,10 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-  System.out.println( "abc!" );
-
-  System.out.println( "xyz" );
+        
 String c =0;
 
     }
